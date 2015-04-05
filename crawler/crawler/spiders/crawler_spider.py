df1cbc700684905128ac9537da2296feeea68f2a
Ayush Maheshwari <akmahesh@asu.edu>
2015-04-05 15:03:36 -0700

    crawler: crawler running with arguments

--- crawler/crawler/spiders/crawler_spider.py
+++ crawler/crawler/spiders/crawler_spider.py
@@ -5,19 +5,18 @@ import sys
 
 class CrawlerSpider(BaseSpider):
     name = "crawler"
-    allowed_domains = ''
-    start_urls = ''
 
     def __init__(self, domain=None, start_urls=None):
-        self.allowed_domains = domain
-	self.start_urls = start_urls 
+        self.allowed_domains = [domain]
+	self.start_urls = [start_urls] 
 
     def parse(self, response):
+        print "Entered"
         hxs = HtmlXPathSelector(response)
         titles = hxs.select("//input")
         item = CrawlerItem()
-        item["form"] = ''
-        item["url"] = sys.argv[2]
+        item["form"] = []
+        item["url"] = response.url
         for title in titles:
             item["form"].append(title.extract())
         return item
