81d7a0ea2e4722b6b6a1a8bfbf7ae390ef803a7f
Kartheek Nallepalli <kartheek.nallepalli@gmail.com>
2015-04-05 16:21:35 -0700

    added way to send cookies

--- crawler/crawler/spiders/crawler_spider.py
+++ crawler/crawler/spiders/crawler_spider.py
@@ -2,21 +2,27 @@ from crawler.crawler_items import CrawlerItem
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 import sys
+import scrapy
+import ast
 
 class CrawlerSpider(BaseSpider):
     name = "crawler"
 
-    def __init__(self, domain=None, start_urls=None):
+    def __init__(self, domain=None, start_urls=None, cookies=None):
         self.allowed_domains = [domain]
-	self.start_urls = [start_urls] 
+        self.start_urls = [start_urls]        
+        self.cookies = ast.literal_eval(cookies)	      	      
+	      
+    def start_requests(self):
+        return [scrapy.Request(url=self.start_urls[0], cookies=self.cookies, callback=self.parse_2)]
 
-    def parse(self, response):
+    def parse_2(self, response):        
         print "Entered"
-        hxs = HtmlXPathSelector(response)
-        titles = hxs.select("//input")
         item = CrawlerItem()
+        hxs = HtmlXPathSelector(response)
+        titles = hxs.select("//input")        
         item["form"] = []
         item["url"] = response.url
         for title in titles:
-            item["form"].append(title.extract())
+            item["form"].append(title.extract())            
         return item
